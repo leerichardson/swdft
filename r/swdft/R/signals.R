f2a69c47c6a96989d28ba91939b7400e9bf84c88
leerichardson <leerichardson2013@gmail.com>
2018-06-21 16:03:57 -0400

    updating documentations and DESCRIPTION+NAMESPACE

--- r/swdft/R/signals.R
+++ r/swdft/R/signals.R
@@ -1,8 +1,32 @@
+#' Local Periodic Signal
+#'
+#' @param N signal length
+#' @param A Amplitude
+#' @param Fr Frequency: Number of cycles in a length N period
+#' @param phase phase
+#' @param S start of local signal
+#' @param L length of local signal
+#'
+#' @export
+#'
+#' @return length N local periodic signal
+#'
+local_signal <- function(N, A=1, Fr=1, phase=0, S=0, L=N) {
+  periodic_signal <- cosine(N, A=A, Fr=Fr, phase=phase)
+
+  indicator <- rep(0, N)
+  indicator[(S + 1):(S + L)] <- 1
+
+  periodic_signal * indicator
+}
+
 #' The principal nth root of unity
 #'
 #' @param n integer root
 #'
-#' @return the resulting complex number
+#' @export
+#'
+#' @return complex number
 #'
 prou <- function(n) {
   i <- complex(length.out = 1, real = 0, imaginary = 1)
@@ -35,24 +59,4 @@ cosine <- function(N, A=1, Fr=1, phase=0) {
 sine <- function(N, A=1, Fr=1, phase=0) {
   x <- 0:(N - 1)
   A * sine( ((2 * pi * Fr * x) / N) + phase )
-}
-
-#' Local Periodic Signal
-#'
-#' @param N signal length
-#' @param A Amplitude
-#' @param Fr Frequency: Number of cycles in a length N period
-#' @param phase phase
-#' @param S start of local signal
-#' @param L length of local signal
-#'
-#' @return length N local periodic signal
-#'
-local_signal <- function(N, A=1, Fr=1, phase=0, S=0, L=N) {
-  periodic_signal <- cosine(N, A=A, Fr=Fr, phase=phase)
-
-  indicator <- rep(0, N)
-  indicator[(S + 1):(S + L)] <- 1
-
-  periodic_signal * indicator
 }
