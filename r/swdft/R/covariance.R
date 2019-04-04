07bb0db8606a77be89b4a2d55fd65ed9888396bf
leerichardson <leerichardson2013@gmail.com>
2019-04-04 09:52:10 -0400

    final covariance derivations used in dissertation

--- r/swdft/R/covariance.R
+++ r/swdft/R/covariance.R
@@ -36,35 +36,3 @@ cov_swdft_ir <- function(k, l, delta, n, sigma) {
   shift2 <- Im( swdft::dirichlet(x=(-2*pi*(k-l))/n, phase=(-2*pi*delta*l)/n, a=delta, b=n-1) )
   return( (sigma^2 / (2*n)) * (shift1 + shift2) )
 }
-
-#' #' Covariance between two complex-numbered outputs
-#' #'
-#' #' @param k
-#' #' @param l
-#' #' @param delta
-#' #' @param n
-#' #' @param sigma
-#' #'
-#' #' @return real-valued covariance between squared modulus SWDT coefficients
-#' #'
-#' cov_swdft_sqmod <- function(k, l, delta, n, sigma) {
-#'   case1 <- 3 * (n - delta)
-#'   case2 <- (n^2 - (n - delta))
-#'
-#'   case3 <- complex(length.out=1, real=0, imaginary=0)
-#'   case4 <- complex(length.out=1, real=0, imaginary=0)
-#'   srange <- delta:(n-1)
-#'   vrange <- delta:(n-1)
-#'   for (s in srange) {
-#'     for (v in vrange) {
-#'       indicator <- ifelse(s==v, 0, 1)
-#'       case3_pow <- (k + l) * (v - s)
-#'       case4_pow <- (k * (v - s)) + (l * (s - v))
-#'       case3 <- case3 + (indicator * prou(n=n)^( -case3_pow ))
-#'       case4 <- case4 + (indicator * prou(n=n)^( -case4_pow ))
-#'     }
-#'   }
-#'
-#'   part1 <- (sigma^4 / n^2) * (case1 + case2 + Re(case3) + Re(case4))
-#'   return(part1 - sigma^4)
-#' }
