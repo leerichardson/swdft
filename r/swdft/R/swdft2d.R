8ea49e492863c8055f33e9a874d2f40feb2a25c0
leerichardson <leerichardson2013@gmail.com>
2019-04-09 08:45:44 -0400

    added constructors+tests for 2D SWDFT

---
+++ r/swdft/R/swdft2d.R
@@ -0,0 +1,89 @@
+#' 2D Sliding Window Discrete Fourier Transform
+#'
+#' @param x 2D input signal
+#' @param n0 window size in row direction
+#' @param n1 window size in column direction
+#' @param type algorithm to implement. defaults to "fftw", other option 'fft' for R's base FFT function.
+#' R's base fft function is used if
+#'
+#' @return An S3 'swdft2d' object. See ?new_swdft for details.
+#'
+swdft2d <- function(x, n0, n1, type="fftw") {
+  if (type == "fftw") {
+    if (requireNamespace("fftwtools", quietly = TRUE)) {
+      a <- swdft2d_fftw(x, n0, n1) # Run with the 'fftwtools' library
+    } else {
+      print("Using base fft, since fftwtools package is not available")
+      a <- swdft2d_fft(x, n0, n1) # Run with base R's 'fft' function
+    }
+  } else if (type == "fft") {
+    a <- swdft2d_fft(x, n0, n1)
+  } else {
+    stop("type must be 'base' or 'fftw'")
+  }
+
+  ## Return a 'swdft2d' S3 object
+  swdft2d_obj <- swdft::new_swdft2d(a=a, x=x, n0=n0, n1=n1, type=type)
+  return( swdft2d_obj )
+
+  return(a)
+}
+
+#' Constructor function for class 'swdft2d'
+#'
+#' @param a 4D complex-valued array of 2D SWDFT coefficients
+#' @param x 2D real or complex valued signal
+#' @param n0 window size in row direction
+#' @param n1 window size in column direction
+#' @param type algorithm to implement. defaults to "fftw", other option 'fft' for R's base FFT function.
+#' R's base fft function is used if
+#'
+#' @return S3 object w/ the same elements as arguments to this constructor function
+#'
+new_swdft2d <- function(a, x, n0, n1, type) {
+  structure(list(a=a, x=x, n0=n0, n1=n1, type=type), class=c("swdft2d", "swdft"))
+}
+
+#' 2D Sliding Window Discrete Fourier Transform using fftw
+#'
+#' @param x 2D input signal
+#' @param n0 window size in row direction
+#' @param n1 window size in column direction
+#'
+swdft2d_fftw <- function(x, n0, n1) {
+  N0 <- dim(x)[1]
+  N1 <- dim(x)[2]
+  P0 <- N0 - n0 + 1
+  P1 <- N1 - n1 + 1
+  a <- array(data=NA, dim=c(n0, n1, P0, P1))
+
+  for (p0 in n0:N0) {
+    for (p1 in n1:N1) {
+      a[, , p0 - n0 + 1, p1 - n1 + 1] <- fftwtools::fftw2d(x[(p0 - n0 + 1):p0, (p1 - n1 + 1):p1])
+    }
+  }
+
+  return(a)
+}
+
+#' 2D Sliding Window Discrete Fourier Transform using base R
+#'
+#' @param x 2D input signal
+#' @param n0 window size in row direction
+#' @param n1 window size in column direction
+#'
+swdft2d_fft <- function(x, n0, n1) {
+  N0 <- dim(x)[1]
+  N1 <- dim(x)[2]
+  P0 <- N0 - n0 + 1
+  P1 <- N1 - n1 + 1
+  a <- array(data=NA, dim=c(n0, n1, P0, P1))
+
+  for (p0 in n0:N0) {
+    for (p1 in n1:N1) {
+      a[, , p0 - n0 + 1, p1 - n1 + 1] <- stats::fft(x[(p0 - n0 + 1):p0, (p1 - n1 + 1):p1])
+    }
+  }
+
+  return(a)
+}
---
+++ r/swdft/tests/testthat/test-swdft2d.R
@@ -0,0 +1,29 @@
+context("2D and 3D SWDFTs work")
+
+test_that("2D, 3D SWDFT Algorithms give the same answer", {
+  # --- 2D SWDFT ----
+  N0 <- 40
+  N1 <- 40
+  n0 <- 16
+  n1 <- 16
+
+  data <- complex(N0 * N1, rnorm(N0 * N1), rnorm(N0 * N1))
+  x <- array(data = data, dim = c(N0, N1))
+
+  a_base <- swdft2d(x=x, n0=n0, n1=n1, type="fft")
+  a_fftw <- swdft2d(x=x, n0=n0, n1=n1, type="fftw")
+
+  expect_true( all( round( Re(a_base$a - a_fftw$a), digits = 5) == 0) )
+  expect_true( all( round( Im(a_base$a - a_fftw$a), digits = 5) == 0) )
+
+  # --- 3D SWDFT ---
+  N0 <- 20
+  N1 <- 20
+  N2 <- 20
+  n0 <- 8
+  n1 <- 8
+  n2 <- 8
+  data <- complex(length.out= N0 * N1 * N2, real=rnorm(N0 * N1 * N2), imaginary=rnorm(N0 * N1 * N2))
+  x <- array(data=data, dim=c(N0, N1, N2))
+  a_base <- swdft3d(x=x, n0=n0, n1=n1, n2=n2)
+})
